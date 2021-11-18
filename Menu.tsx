f05d59966dce2667a94eb05e98a4b93034e4f8a9
alonsomoslie777 <alonsomoslie777@gmail.com>
2021-11-18 15:41:47 +0100

    1 file modified

--- Menu.tsx
+++ Menu.tsx
@@ -8,11 +8,11 @@ class Menu extends Info {
       lang: 'en',
       row: {},
       fields: {},
-      json: '{"autoload":null,"param":"website_menu","value":"Footer menu","value1":"{"en":"<li><a href=\\"#\\" class=\\"simple_link\\">Categories</a><ul><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/cat/clothing\\" class=\\"simple_link\\">Clothing</a></li><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/cat/handbags\\" class=\\"simple_link\\">Handbags</a></li><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/cat/electronics\\" class=\\"simple_link\\">Electronics</a></li><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/cat/software\\" class=\\"simple_link\\">Software</a></li></ul></li><li class=\\"\\"><a href=\\"#\\" class=\\"simple_link\\">Quick Links</a><ul><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/orders-list\\" class=\\"simple_link\\">My orders</a></li><li><a href=\\"/cart/get\\" class=\\"simple_link\\">Cart</a></li><li><a href=\\"/checkout/step1\\" class=\\"simple_link\\">Checkout</a></li><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/wishlist\\" class=\\"simple_link\\">Wishlist</a></li><li><a href=\\"/signup/my-account\\" class=\\"simple_link\\">My Account</a></li></ul></li><li style=\\"opacity: 1;\\" class=\\"activeIn\\"><a href=\\"#\\" class=\\"simple_link\\">Policies</a><ul><li><a href=\\"/page/privacy-policy\\" class=\\"simple_link\\">Privacy Policy</a></li><li><a href=\\"/page/terms--conditions\\" class=\\"simple_link\\">Terms &amp; Conditions</a></li><li><a href=\\"/page/returns-policy\\" class=\\"simple_link\\">Returns Policy</a></li><li><a href=\\"/page/size-guide\\" class=\\"simple_link\\">Size Guide</a></li></ul></li>"}","value2":"footer"}',
+      json: '{"autoload":null,"param":"website_menu","value":"Footer menu","value1": {"en":"<li><a href=\\"#\\" class=\\"simple_link\\">Categories</a><ul><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/cat/clothing\\" class=\\"simple_link\\">Clothing</a></li><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/cat/handbags\\" class=\\"simple_link\\">Handbags</a></li><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/cat/electronics\\" class=\\"simple_link\\">Electronics</a></li><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/cat/software\\" class=\\"simple_link\\">Software</a></li></ul></li><li class=\\"\\"><a href=\\"#\\" class=\\"simple_link\\">Quick Links</a><ul><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/orders-list\\" class=\\"simple_link\\">My orders</a></li><li><a href=\\"/cart/get\\" class=\\"simple_link\\">Cart</a></li><li><a href=\\"/checkout/step1\\" class=\\"simple_link\\">Checkout</a></li><li style=\\"opacity: 1;\\" class=\\"\\"><a href=\\"/wishlist\\" class=\\"simple_link\\">Wishlist</a></li><li><a href=\\"/signup/my-account\\" class=\\"simple_link\\">My Account</a></li></ul></li><li style=\\"opacity: 1;\\" class=\\"activeIn\\"><a href=\\"#\\" class=\\"simple_link\\">Policies</a><ul><li><a href=\\"/page/privacy-policy\\" class=\\"simple_link\\">Privacy Policy</a></li><li><a href=\\"/page/terms--conditions\\" class=\\"simple_link\\">Terms &amp; Conditions</a></li><li><a href=\\"/page/returns-policy\\" class=\\"simple_link\\">Returns Policy</a></li><li><a href=\\"/page/size-guide\\" class=\\"simple_link\\">Size Guide</a></li></ul></li>"},"value2":"footer"}',
     };
   }
   componentDidMount() {
-    alert(this.state.json);
+    //alert(this.state.json);
     this.setState({ row: JSON.parse(this.state.json) });
     //console.log('json is:', this.state.fields);
   }
@@ -22,165 +22,6 @@ class Menu extends Info {
       <div class="col-md-12">
         <div class="col-md-12">
           <h3>{this.state.row.value}</h3>
-        </div>
-
-        <div class="col-sm-4">
-          <div class="card">
-            <div class="block">
-              <h3 class="header_block menu_open">{this.l('Custom Link')}</h3>
-              <div class="paddinginside block_inside active">
-                <label class="col-md-4">{this.l('Select Icon')}</label>
-                <div class="col-md-8">
-                  <button
-                    type="button"
-                    class="icon_link"
-                    onclick="icon_setup('.link_text',this)"
-                    data-toggle="modal"
-                    data-target="#load_icons_modal"
-                  >
-                    Select icon
-                  </button>
-                </div>
-                <div class="height10px"></div>
-
-                <label class="col-md-4">{this.l('Select URL')}</label>
-                <div class="col-md-8">
-                  <select
-                    name=""
-                    class="form-control"
-                    onchange="select_url_fromlist(this,'.link_text_href','.link_text')"
-                  >
-                    <option value="">{this.l('Select')}</option>
-                    <option value={'key'}> </option>
-                  </select>
-                </div>
-                <div class="height10px"></div>
-                <label class="col-md-4">{this.l('Or enter manually')}</label>
-
-                <div class="col-md-8">
-                  <input
-                    type="text"
-                    value=""
-                    placeholder="#"
-                    name="link"
-                    class="form-control link_text_href"
-                  />
-                </div>
-
-                <div class="height10px"></div>
-                <label class="col-md-4">{this.l('Menu name')}</label>
-                <div class="col-md-8">
-                  <input
-                    type="text"
-                    value=""
-                    placeholder="Home"
-                    name="link"
-                    class="form-control col-md-8 link_text"
-                    data-icons=""
-                  />
-                </div>
-
-                <div class="height10px"></div>
-                <p class="col-md-12 text_align_right">
-                  <a
-                    href="#"
-                    class="btn btn_small"
-                    onclick="return simple_link('.link_text','text');"
-                  >
-                    {this.l('Add ittem')}
-                  </a>
-                </p>
-                <div class="clear"></div>
-              </div>
-            </div>
-
-            <div class="block">
-              <h3 class="header_block menu_open">{this.l('Pages')}</h3>
-              <div class="paddinginside block_inside">
-                <label class="col-md-4">{this.l('Select Icon')}</label>
-                <div class="col-md-8">
-                  <button
-                    type="button"
-                    class="icon_pages"
-                    onclick="icon_setup('.link_page',this)"
-                    data-toggle="modal"
-                    data-target="#load_icons_modal"
-                  >
-                    {this.l('Select icon')}
-                  </button>
-                </div>
-
-                <div class="height10px"></div>
-
-                <label class="col-md-4">{this.l('Page')}</label>
-                <div class="col-md-8">
-                  <select class="form-control link_page" data-icons="">
-                    <option
-                      value={'/page/{value.cpu()}'}
-                      data-info={'page_{value.pageid()}'}
-                    >
-                      {'value.title()'}
-                    </option>
-                  </select>
-                </div>
-                <div class="height10px"></div>
-                <p class="col-md-12 text_align_right">
-                  <a
-                    href="#"
-                    class="btn btn_small"
-                    onclick="return simple_link('.link_page', '.page_icon');"
-                  >
-                    {this.l('Add ittem')}
-                  </a>
-                </p>
-                <div class="clear"></div>
-              </div>
-            </div>
-
-            <div class="block">
-              <h3 class="header_block menu_open">{this.l(value)}</h3>
-              <div class="paddinginside block_inside">
-                <label class="col-md-4">{this.l('Select Icon')}</label>
-                <div class="col-md-8">
-                  <button
-                    type="button"
-                    class="{class1}"
-                    onclick="icon_setup('.{class2}',this)"
-                    data-toggle="modal"
-                    data-target="#load_icons_modal"
-                  >
-                    {this.l('Select icon')}
-                  </button>
-                </div>
-
-                <div class="height10px"></div>
-
-                <label class="col-md-4">{this.l('Select')}</label>
-                <div class="col-md-8">
-                  <select class="form-control {class2}" data-icons="">
-                    <option
-                      data-info={'page_{key2}'}
-                      value={
-                        "<#if (key?contains('product') || key?contains('brand'))>/cat/<#else>/categories/</#if>{value2.Cpufull()}"
-                      }
-                    >
-                      {'{value.title}'}
-                    </option>
-                  </select>
-                </div>
-                <div class="height10px"></div>
-                <p class="col-md-12 text_align_right">
-                  <a href="#" class="btn btn_small">
-                    {this.l('Add all')}
-                  </a>
-                  <a href="#" class="btn btn_small">
-                    {this.l('Add ittem')}
-                  </a>
-                </p>
-                <div class="clear"></div>
-              </div>
-            </div>
-          </div>
         </div>
 
         <div class="col-sm-8">
@@ -223,11 +64,11 @@ class Menu extends Info {
                 <textarea
                   name="munuIttem"
                   id="response"
-                  style="display: none;"
+                  style={{ display: 'none' }}
                 ></textarea>
 
                 <div class="col-md-12">
-                  <div class="col-md-3" style="float: right;">
+                  <div class="col-md-3" style={{ float: 'right' }}>
                     <select
                       onchange="if (this.value) window.location.href=this.value"
                       class="form-control"
