dfe399b9079aefacfff72e21f4e0c89701c8e8fe
alonsomoslie777 <alonsomoslie777@gmail.com>
2021-11-18 15:51:02 +0100

    1 file modified

--- Menu.tsx
+++ Menu.tsx
@@ -23,7 +23,165 @@ class Menu extends Info {
         <div class="col-md-12">
           <h3>{this.state.row.value}</h3>
         </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="block">
+              <h3 class="header_block menu_open">{this.l('Custom Link')}</h3>
+              <div class="paddinginside block_inside active">
+                <label class="col-md-4">{this.l('Select Icon')}</label>
+                <div class="col-md-8">
+                  <button
+                    type="button"
+                    class="icon_link"
+                    onclick="icon_setup('.link_text',this)"
+                    data-toggle="modal"
+                    data-target="#load_icons_modal"
+                  >
+                    Select icon
+                  </button>
+                </div>
+                <div class="height10px"></div>
 
+                <label class="col-md-4">{this.l('Select URL')}</label>
+                <div class="col-md-8">
+                  <select
+                    name=""
+                    class="form-control"
+                    onchange="select_url_fromlist(this,'.link_text_href','.link_text')"
+                  >
+                    <option value="">{this.l('Select')}</option>
+                    <option value={'key'}>aaaaaa </option>
+                  </select>
+                </div>
+                <div class="height10px"></div>
+                <label class="col-md-4">{this.l('Or enter manually')}</label>
+
+                <div class="col-md-8">
+                  <input
+                    type="text"
+                    value=""
+                    placeholder="#"
+                    name="link"
+                    class="form-control link_text_href"
+                  />
+                </div>
+
+                <div class="height10px"></div>
+                <label class="col-md-4">{this.l('Menu name')}</label>
+                <div class="col-md-8">
+                  <input
+                    type="text"
+                    value=""
+                    placeholder="Home"
+                    name="link"
+                    class="form-control col-md-8 link_text"
+                    data-icons=""
+                  />
+                </div>
+
+                <div class="height10px"></div>
+                <p class="col-md-12 text_align_right">
+                  <a
+                    href="#"
+                    class="btn btn_small"
+                    onclick="return simple_link('.link_text','text');"
+                  >
+                    {this.l('Add ittem')}
+                  </a>
+                </p>
+                <div class="clear"></div>
+              </div>
+            </div>
+
+            <div class="block">
+              <h3 class="header_block menu_open">{this.l('Pages')}</h3>
+              <div class="paddinginside block_inside">
+                <label class="col-md-4">{this.l('Select Icon')}</label>
+                <div class="col-md-8">
+                  <button
+                    type="button"
+                    class="icon_pages"
+                    onclick="icon_setup('.link_page',this)"
+                    data-toggle="modal"
+                    data-target="#load_icons_modal"
+                  >
+                    {this.l('Select icon')}
+                  </button>
+                </div>
+
+                <div class="height10px"></div>
+
+                <label class="col-md-4">{this.l('Page')}</label>
+                <div class="col-md-8">
+                  <select class="form-control link_page" data-icons="">
+                    <option
+                      value={'/page/value.cpu'}
+                      data-info={'page_value.pageid'}
+                    >
+                      {'value.title'}
+                    </option>
+                  </select>
+                </div>
+                <div class="height10px"></div>
+                <p class="col-md-12 text_align_right">
+                  <a
+                    href="#"
+                    class="btn btn_small"
+                    onclick="return simple_link('.link_page', '.page_icon');"
+                  >
+                    {this.l('Add ittem')}
+                  </a>
+                </p>
+                <div class="clear"></div>
+              </div>
+            </div>
+
+            <div class="block">
+              <h3 class="header_block menu_open">{this.l('cat name')}</h3>
+              <div class="paddinginside block_inside">
+                <label class="col-md-4">{this.l('Select Icon')}</label>
+                <div class="col-md-8">
+                  <button
+                    type="button"
+                    class="{class1}"
+                    onclick="icon_setup('.{class2}',this)"
+                    data-toggle="modal"
+                    data-target="#load_icons_modal"
+                  >
+                    {this.l('Select icon')}
+                  </button>
+                </div>
+
+                <div class="height10px"></div>
+
+                <label class="col-md-4">{this.l('Select')}</label>
+                <div class="col-md-8">
+                  <select class="form-control {class2}" data-icons="">
+                    <option
+                      data-info={'page_{key2}'}
+                      value={
+                        '(key=="product" || key=="brand"  ? "/cat/": "categories")+value2.Cpufull()'
+                      }
+                    >
+                      {'{value.title}'}
+                    </option>
+                  </select>
+                </div>
+                <div class="height10px"></div>
+                <p class="col-md-12 text_align_right">
+                  <a href="#" class="btn btn_small">
+                    {this.l('Add all')}
+                  </a>
+                  <a href="#" class="btn btn_small">
+                    {this.l('Add ittem')}
+                  </a>
+                </p>
+                <div class="clear"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/****************************** */}
         <div class="col-sm-8">
           <form action="#" method="post" enctype="multipart/form-data">
             <div class="card">
